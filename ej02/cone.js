6ad1f162ee6af591305ed51ab8433d0936eab4c0
lm-duque <123669002+lm-duque@users.noreply.github.com>
2023-03-30 16:05:55 -0500

    Add files via upload

---
+++ ej02/cone.js
@@ -0,0 +1,57 @@
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor(0xDDDDDD, 1);
+document.body.appendChild(renderer.domElement);
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(80, WIDTH / HEIGHT);
+camera.position.z = 4.5;
+camera.position.x = -1.2;
+camera.position.y = 2;
+
+camera.rotation.set(0, -0.5, 0);
+scene.add(camera);
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Cono
+
+var geometry = new THREE.ConeGeometry(1, 3, 10);
+var material = new THREE.MeshPhongMaterial({ color: 0xCC99FF });
+var cone = new THREE.Mesh(geometry, material);
+scene.add(cone)
+cone.position.y = 1.5
+
+//Rotacion
+
+geometry.rotateX(Math.PI/2);
+
+//Transladar
+
+geometry.translate(0,-0.5,1.5 );
+
+geometry.rotateX(Math.PI/2);
+
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+const size = 150;
+const divisions = 160;
+const axesHelper = new THREE.AxesHelper(1000);
+scene.add(axesHelper);
+
+const gridHelper = new THREE.GridHelper(size, divisions);
+scene.add(gridHelper);
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+render();
